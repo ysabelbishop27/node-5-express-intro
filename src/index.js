79bec732ac2a7201d7aa196a790282f025409214
ysabelbishop27 <ysabelbishop5150@gmail.com>
2026-07-17 14:46:16 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,28 +1,64 @@
 // ---------------------------------
 // Boilerplate Code to Set Up Server
 // ---------------------------------
-
+import express from "express";
+import config from "./config.js";
+const db = new pg.Pool({
+  connectionString: config.databaseUrl,
+  ssl: true,
+});
+const app = express();
+app.use(express.json());
+const port = 3000; 
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
 // ---------------------------------
 // Helper Functions
 // ---------------------------------
+function sayHello(name, language) {
+  const selectedLanguage = language.toLowerCase();
 
+  if (selectedLanguage === "english") {
+    return `Hello, ${name}!`;
+  } else if (selectedLanguage === "spanish") {
+    return `Hola, ${name}!`;
+  } else if (selectedLanguage === "vietnamese") {
+    return `Xin chao, ${name}!`;
+  } else if (selectedLanguage === "turkish") {
+    return `Merhaba, ${name}!`;
+  } else {
+    return "Language not supported.";
+  }
+}
 // ---------------------------------
 // Our very first API Endpoints
 // ---------------------------------
-
+app.get("/api", (req, res) => {
+  res.send("Welcome to my server!");
+});
 // --------------------------------
 // 🚀 LEVEL 1 CHALLENGES 
 // --------------------------------
 
 // 1. 🏆 Add a /goodbye endpoint that responds with "Goodbye, see you later!"
-
+app.get("/api/goodbye", (req, res) => {
+  res.send("Goodbye, see you later!");
+});
 // 2. 🏆 Add a /happy-birthday endpoint that responds with "Happy birthday!!!"
-
+app.get("/api/happy-birthday", (req, res) => {
+  res.send("Happy birthday!!!");
+});
 // --------------------------------
 // 🚀 LEVEL 2 CHALLENGES — ADDING DYNAMIC PARAMETERS
 // --------------------------------
 
 // 1. 🏆 Add a /happy-birthday/:name endpoint says "Happy birthday, [name]!!!"
+app.get("/api/happy-birthday/:name", (req, res) => {
+  const name = req.params.name;
+
+  res.send(`Happy birthday, ${name}!!!`);
+});
 
 // 2. 🏆 Add a /say-hello/:name/:language endpoint that says hello in multiple languages. Examples:
 //      - If language = "English", respond with "Hello, [name]!"
@@ -31,7 +67,13 @@
 //      - If language = "Turkish", respond with "Merhaba, [name]!"
 //      - Add as many languages as you want! 
 //      - Otherwise, respond with "Language not supported."" 
+app.get("/api/say-hello/:name/:language", (req, res) => {
+  const { name, language } = req.params;
 
+  const message = sayHello(name, language);
+
+  res.send(message);
+});
 // --------------------------------
 // 🚀 LEVEL 3 CHALLENGES — EVEN MORE DYNAMIC PARAMETERS
 // --------------------------------
